9b59585ccdcfc4e4b0dc4e6db022ec5167069a07
9boom <9boom2008@gmail.com>
2026-08-15 14:49:48 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 80
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public'), {
+  extensions: ['html']
+}));
+
+app.get('/api/info', (req, res) => {
+  res.json({
+    status: 'success',
+    message: 'Connected'
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404 - Page not found</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://<all-interfaces>:${PORT}`);
+}); 
